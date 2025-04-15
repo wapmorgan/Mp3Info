deca17d88c45e4164e32343961f2bc883e5a6bac
Romchik38 <pomahehko.c@gmail.com>
2025-04-15 17:40:46 +0300

    tests: added test for static isValidAudio

---
+++ tests/Unit/Mp3InfoTest.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace wapmorgan\Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+use wapmorgan\Mp3Info\Mp3Info;
+
+final class Mp3InfoTest extends TestCase
+{
+    public function testIsValidAudio(): void
+    {
+        $file =  __DIR__ . '/Samples/kky3OREM.mp3';
+    
+        $this->assertSame(true, Mp3Info::isValidAudio($file));
+    }
+}
